a8abc6201bd96ae246c63592887a1b5249dfd1c9
Arif Hossain <arif04cuet@gmail.com>
2021-09-26 10:53:43 +0600

    associate memebr application emails added

--- plugins/techpanda/core/classes/FrontendEvents.php
+++ plugins/techpanda/core/classes/FrontendEvents.php
@@ -10,6 +10,27 @@ class FrontendEvents
     public function subscribe($events)
     {
         $events->listen('online.application.submitted', 'Techpanda\Core\Classes\FrontendEvents@onlineApplicationSubmittedHandler');
+        $events->listen('associate.application.submitted', 'Techpanda\Core\Classes\FrontendEvents@associateApplicationSubmittedHandler');
+    }
+
+    public function associateApplicationSubmittedHandler($user)
+    {
+        // send emails to user and admin
+        $data = $user->toArray();
+        $data['fullName'] = $user->full_name;
+        $data['link'] = config('app.url') . '/backend/backend/users/update/' . $user->id;
+
+        //send to user
+        $template = 'techpanda.core::mail.associate_application_submitted_to_user';
+        Mail::send($template, $data, function ($message) use ($user) {
+            $message->to($user->email, $user->full_name);
+        });
+
+        //send to admin
+        $template = 'techpanda.core::mail.associate_application_submitted_to_admin';
+        Mail::send($template, $data, function ($message) use ($user) {
+            $message->to('bcsp60ftccoop@gmail.com', 'P60FTC Cooperative');
+        });
     }
 
     public function onlineApplicationSubmittedHandler($user)
@@ -28,7 +49,7 @@ class FrontendEvents
         //send to admin
         $template = 'techpanda.core::mail.online_application_submitted_to_admin';
         Mail::send($template, $data, function ($message) use ($user) {
-            $message->to('bcsp60ftccoop@gmail.com ', 'P60FTC Cooperative');
+            $message->to('bcsp60ftccoop@gmail.com', 'P60FTC Cooperative');
         });
     }
 }
